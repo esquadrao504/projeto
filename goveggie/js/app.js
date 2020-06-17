2c6961495512c40742835632288c4e985027ce7c
esquadrao504 <66097370+esquadrao504@users.noreply.github.com>
2020-06-17 16:29:45 -0300

    goveggie

---
+++ goveggie/js/app.js
@@ -0,0 +1,10 @@
+/* Alerta inscrição */
+
+$(document).ready(function () {
+    $('.site-btn').on('click', function () {
+        bootbox.alert('Inscrição realizada com sucesso! Você receberá todas as notícias em seu e-mail.') 
+    })
+})
+
+
+
